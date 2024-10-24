6f65e8b0ab0cafc373ebe5f06edc4165be05c14a
vinicius-leonel <viniciuslps.cms@gmail.com>
2024-10-24 14:00:17 -0300

    test: usando o Maven para executar os testes

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "steps",
+        glue = {"steps", "hook"},
         tags = "@regressivo",
         plugin = {"html:target/cucumber-reports.html"}
 )
